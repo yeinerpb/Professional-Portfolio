10493c0e070955f34ad4ee3d2bedd48086b21a73
Yeiner Parra <yeinerparrabernal@gmail.com>
2024-11-04 21:37:09 -0500

    Se ingreso un nuevo cv, se quito la opcion de cambiar de idioma

--- js/theme.js
+++ js/theme.js
@@ -1,20 +1,17 @@
 /*======= Dark Mode ========*/
 let darkModeIcon = document.querySelector("#darkMode-icon");
-// obtener el estado del tema del almacenamiento local
-let darkMode = localStorage.getItem("darkMode"); 
+let darkMode = localStorage.getItem("darkMode");
 
 if (darkMode === "true") {
-    // si el tema es oscuro, aplicar las clases correspondientes
-    darkModeIcon.classList.add("bx-sun");
-    document.body.classList.add("dark-mode");
+  darkModeIcon.classList.add("bx-sun");
+  document.body.classList.add("dark-mode");
 }
 
 darkModeIcon.onclick = () => {
-    // al hacer clic en el icono, cambiar el tema y guardar el estado
-    darkModeIcon.classList.toggle("bx-sun");
-    document.body.classList.toggle("dark-mode");
-    localStorage.setItem(
-        "darkMode",
-        document.body.classList.contains("dark-mode")
-    ); // guardar el estado del tema como un valor booleano
+  darkModeIcon.classList.toggle("bx-sun");
+  document.body.classList.toggle("dark-mode");
+  localStorage.setItem(
+    "darkMode",
+    document.body.classList.contains("dark-mode")
+  );
 };
